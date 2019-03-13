eb8efdf9a90b85c61eb3737af9b601f8f9f539e6
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-13 18:32:30 +0900

    前と同じ結果は表示しないじゃんけんアプリの作成

--- MyJanken/MyJanken/ViewController.swift
+++ MyJanken/MyJanken/ViewController.swift
@@ -15,6 +15,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // じゃんけん(数字)
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        var newAnswerNumber = 0
+        
+        repeat {
+            newAnswerNumber = Int(arc4random_uniform(3))
+        } while answerNumber == newAnswerNumber
+        
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
+            // グー
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+        } else if answerNumber == 1 {
+            // チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        } else if answerNumber == 2 {
+            // パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+        }
+        
+    }
+    
+    
+    
 }
 
