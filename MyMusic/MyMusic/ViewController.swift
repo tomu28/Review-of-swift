40b6ff4875248cf8e59d40b438a83875123bd7d8
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-13 23:42:28 +0900

    refactoring

--- MyMusic/MyMusic/ViewController.swift
+++ MyMusic/MyMusic/ViewController.swift
@@ -22,13 +22,9 @@ class ViewController: UIViewController {
 
     @IBAction func cymbal(_ sender: Any) {
     
-        do {
-        cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+        // `&`を変数の前につけると、参照渡しになる
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
         
-        cymbalPlayer.play()
-        } catch {
-            print("シンバルで、エラーが発生しました！")
-        }
     }
 
     let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
@@ -36,35 +32,35 @@ class ViewController: UIViewController {
     
     
     @IBAction func guitar(_ sender: Any) {
-        do {
-            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            guitarPlayer.play()
-        } catch {
-            print("ギターで、エラーが発生しました!")
-        }
+        
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+        
     }
     
     let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
     var backmusicPlayer = AVAudioPlayer()
     
     @IBAction func play(_ sender: Any) {
-        do {
-            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil
-            )
-            
-            // リピート再生
-            backmusicPlayer.numberOfLoops = -1
-            backmusicPlayer.play()
-            
-        } catch{
-            print("エラーが発生しました！")
-        }
+        
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
+        
     }
     
     @IBAction func stop(_ sender: Any) {
     
         backmusicPlayer.stop()
         
+    }
+    
+    // 共有プレイヤーの再生処理
+    fileprivate func soundPlayer(player: inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました〜！")
+        }
     }
     
     
