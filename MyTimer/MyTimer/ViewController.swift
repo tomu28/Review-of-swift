59375f62b50873317216510a84cd2fa9e4924f14
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-14 15:31:01 +0900

    時間になったらダイアログを表示する機能を実装

--- MyTimer/MyTimer/ViewController.swift
+++ MyTimer/MyTimer/ViewController.swift
@@ -82,6 +82,15 @@ class ViewController: UIViewController {
         if displayUpdate() <= 0 {
             count = 0
             timer.invalidate()
+
+            // ダイアログを表示
+                // 下からスライドして表示される。　　preferredStyle: .alert  とすると、ホップアップするダイアログが表示される
+            let alertController = UIAlertController(title: "終了", message: "タイマー終了時間です", preferredStyle: .actionSheet)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+            
+            
         }
     }
     
