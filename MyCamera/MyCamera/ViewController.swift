c062e41a328bdb71560347ca76776ea06f67e281
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-14 17:31:07 +0900

    フォトライブラリーから写真を読み込めるように機能を追加

--- MyCamera/MyCamera/ViewController.swift
+++ MyCamera/MyCamera/ViewController.swift
@@ -19,16 +19,54 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var pictureImage: UIImageView!
     
     @IBAction func cameraButtonAction(_ sender: Any) {
+//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            print("カメラは利用できます")
+//            // UIImagePickerControllerのインスタンスを作成
+//            let imagePickerController = UIImagePickerController()
+//            imagePickerController.sourceType = .camera
+//            imagePickerController.delegate = self
+//            present(imagePickerController, animated: true, completion: nil)
+//        } else {
+//            print("カメラは利用できません")
+//        }
+        
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+        
+        // カメラが利用可能かチャック
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("カメラは利用できます")
-            // UIImagePickerControllerのインスタンスを作成
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.sourceType = .camera
-            imagePickerController.delegate = self
-            present(imagePickerController, animated: true, completion: nil)
-        } else {
-            print("カメラは利用できません")
+            
+            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: { (action:UIAlertAction) in
+                
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .camera
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
         }
+        
+        // フォトライブラリーが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: { (action: UIAlertAction) in
+                
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+                
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        
+        // キャンセルの選択肢を定義
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        alertController.popoverPresentationController?.sourceView = view
+        
+        present(alertController, animated: true, completion: nil)
         
     }
     
