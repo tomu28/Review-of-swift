85a8fcac2d38e426c1cb6088b054e1344a028521
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-14 14:05:09 +0900

    Create Project & タイマー画面のレイアウトを作成

---
+++ MyTimer/MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 小幡 十矛 on 2019/03/14.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
