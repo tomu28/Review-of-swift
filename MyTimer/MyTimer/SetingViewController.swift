63f658ef2fd83a3af67a0155c191ba7d7b1d7ba8
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-14 14:19:11 +0900

    UIパーツとプログラムの接続

---
+++ MyTimer/MyTimer/SetingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SetingViewController.swift
+//  MyTimer
+//
+//  Created by 小幡 十矛 on 2019/03/14.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import UIKit
+
+class SetingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+    }
+    
+    
+}
--- MyTimer/MyTimer/ViewController.swift
+++ MyTimer/MyTimer/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopButtonAction(_ sender: Any) {
+    }
+    
 }
 
