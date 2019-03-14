905c4403f4587e935b948d30ca0e16e733e6b9ab
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-03-14 20:15:28 +0900

    JSONをパースし取得

--- MyOkashi/MyOkashi/ViewController.swift
+++ MyOkashi/MyOkashi/ViewController.swift
@@ -29,9 +29,55 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
         if let searchWord = searchBar.text {
             print(searchWord)
+            searchOkashi(keyword: searchWord)
         }
     }
     
+    struct ItemJson: Codable {
+        
+        let name: String?
+        let maker: String?
+        let url: URL?
+        let image: URL?
+    }
+    
+    struct ResultJson: Codable {
+        let item:[ItemJson]
+    }
+    
+    func searchOkashi(keyword : String) {
+        
+        // 検索キーワード
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "http://www.sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else{
+            return
+        }
+        print(req_url)
+    
+        // リクエストに必要な情報を生成
+        let req = URLRequest(url: req_url)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: req,completionHandler: {
+            (data, response, error) in
+            session.finishTasksAndInvalidate()
+            do {
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                
+                print(json)
+            } catch {
+                print("エラーが発生しました")
+            }
+        })
+        // ダウンロード開始
+        task.resume()
+        
+    }
     
 }
 
